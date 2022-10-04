507ca24a580e37ba89d4b2a72085cd6c17279219
Soundryacedcoss <soundryarani@cedcoss.com>
2022-10-04 18:22:22 +0530

    Modification done in task 8

--- arr task-7/script.js
+++ arr task-7/script.js
@@ -1,102 +1,107 @@
-var data1=
-    
-        {name:"samsung",model:"Galaxy",memory:32,price:15000,
-        quantity:20,chk:"<input type='checkbox' id='d1'>",
-    }
+var data1 = {
+  name: "samsung",
+  model: "Galaxy",
+  memory: 32,
+  price: 15000,
+  quantity: 20,
+  chk: "<input type='checkbox' id='d1'>",
+};
 
-var data2=
-        {name:"nokia",model:"S07",memory:64,price:22000,
-        quantity:15,chk:"<input type='checkbox' id='d2'>",
-    }
+var data2 = {
+  name: "nokia",
+  model: "S07",
+  memory: 64,
+  price: 22000,
+  quantity: 15,
+  chk: "<input type='checkbox' id='d2'>",
+};
+
+var data3 = {
+  name: "Xiomi",
+  model: "note",
+  memory: 128,
+  price: 12000,
+  quantity: 21,
+  chk: "<input type='checkbox' id='d3'>",
+};
+
+var data4 = {
+  name: "apple",
+  model: "s01",
+  memory: 64,
+  price: 2500,
+  quantity: 13,
+  chk: "<input type='checkbox' id='d4'>",
+};
+
+var data5 = {
+  name: "motorola",
+  model: "s01",
+  memory: 64,
+  price: 15000,
+  quantity: 18,
+  chk: "<input type='checkbox' id='d5'>",
+};
+var arr = [data1, data2, data3, data4, data5];
+console.log(arr);
+function data() {
+  table =
+    "<table><tr><th>Companny</th><th>Model</th><th>Memory</th><th>price</th><th>Quantity</th><th>Cart</th></tr>";
+  arr.forEach((element) => {
+    table +=
+      "<tr><td>" +
+      element.name +
+      "</td><td>" +
+      element.model +
+      "</td><td>" +
+      element.memory +
+      "</td><td>" +
+      element.price +
+      "</td><td>" +
+      element.quantity +
+      "</td><td>" +
+      element.chk +
+      "</td></tr>";
+  });
+  table += "</table>";
+  document.getElementById("p1").innerHTML = table;
+}
+
+function cart() {
+    
+  if ((arr[0].name = "samsung" && d1.checked)) {
+    arr[0].quantity = arr[0].quantity - 1;
+    display();
+  } else if ((arr[1].name = "nokia" && d2.checked)) {
+    arr[1].quantity = arr[1].quantity - 1;
+    display();
+  } else if ((arr[2].name = "xiomi" && d3.checked)) {
+    arr[2].quantity = arr[2].quantity - 1;
+    display();
+  } else if ((arr[3].name = "apple" && d4.checked)) {
+    arr[3].quantity = arr[3].quantity - 1;
+    display();
+  } else if ((arr[4].name = "moto" && d5.checked)) {
+    arr[4].quantity = arr[4].quantity - 1;
+    display();
+  }
+}
+function display(){
+ var table2="<table><tr><th>Company</th><th>Model</th><th>Memory</th><th>price</th><th>Quantity</th></tr>";
+ arr.forEach(element => {
+    table2+="<tr><td>"+
+    element.name+
+    "</td><td>"+
+    element.model+
+    "</td><td>"+
+    element.memory+
+    "</td><td>"+
+    element.price+
+    "</td><td>"+
+    element.quantity+
+    "</td></tr>"
+ });
    
-var data3=
-        {name:"Xiomi",model:"note",memory:128,price:12000,
-        quantity:21,
-        chk:"<input type='checkbox' id='d3'>",
-    }
-    
-    var data4=
-        {name:"apple",model:"s01",memory:64,price:2500,
-        quantity:13,chk:"<input type='checkbox' id='d4'>",
-    }
-    
-var data5=
-        {name:"motorola",model:"s01",memory:64,price:15000,
-        quantity:18,chk:"<input type='checkbox' id='d5'>",
-    }
-var arr=[data1,data2,data3,data4,data5]
-function fun(){
-    table="<table><tr><th>Companny</th><th>Model</th><th>Memory</th><th>price</th><th>Quantity</th><th>Cart</th></tr>"
-    arr.forEach(element => {
-        table+="<tr><td>"+
-            element.name+
-            "</td><td>"+
-                element.model+
-                "</td><td>"+
-                element.memory+
-                "</td><td>"+
-                element.price+
-                "</td><td>"+
-                element.quantity+
-                "</td><td>"+
-                element.chk+
-            "</td></tr>"
-    });
-    table+="</table>"
-    document.getElementById("p1").innerHTML=table
+   table2+="</table>";
+   document.getElementById("res").innerHTML=table2;
 }
-
-function fun2()
-
-{
-    
-    var quantity
-    if(arr.name="samsung" && d1.checked){
-      quantity=arr[0].quantity-1
-      document.getElementById("p2").innerHTML=" updated quantity of " + arr[0].name +  " is " + quantity
-      
-    }
-    else if(arr.name="nokia" && d2.checked){
-        quantity=arr[1].quantity-1
-        console.log(quantity)
-        document.getElementById("p3").innerHTML=" updated quantity of " + arr[1].name +  " is " + quantity
-        
-    }
-    else if(arr.name="xiomi" && d3.checked){
-        quantity=arr[2].quantity-1
-        console.log(quantity)
-        document.getElementById("p4").innerHTML=" updated quantity of " + arr[2].name +  " is " + quantity
-        
-    }
-    else if(arr.name="apple" && d4.checked){
-        quantity=arr[3].quantity-1
-        console.log(quantity)
-        document.getElementById("p5").innerHTML=" updated quantity of " + arr[3].name +  " is " + quantity
-        
-    }
-    else if(arr.name="moto" && d5.checked){
-        quantity=arr[4].quantity-1
-        console.log(quantity)
-        document.getElementById("p6").innerHTML=" updated quantity of " + arr[4].name +  " is " + quantity
-        
-    }
-    //  var table="<table><tr><th>Companny</th><th>Model</th><th>Memory</th><th>price</th><th>Quantity</th>"
-    //  arr.forEach(element => {
-    //     table+=
-    //     "<tr><td>"
-    //     element.name+
-    //     "</td><td>"+
-    //     element.model+
-    //     "</td><td>"+
-    //     element.memory+
-    //     "</td><td>"+
-    //     element.price+
-    //     "</td><td>"+
-    //     element.quantity
-    //     "</td></tr>"
-    //     document.getElementById("res").innerHTML=table
-    //  });
-    //  table+="</table>"
-    //  document.getElementById("res").innerHTML=table
-}
-
