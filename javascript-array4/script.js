2e52e9c39c2fa93dfbefbfe5a0a3e7a263c79ed8
Soundryacedcoss <soundryarani@cedcoss.com>
2022-10-05 13:33:13 +0530

    task 4 add product at 2nd position

--- javascript-array4/script.js
+++ javascript-array4/script.js
@@ -1,44 +1,38 @@
-var arr1=[
-  {name:"samsung",model:"Galaxy",memory:32,price:15000},
-  {name:"nokia",model:"S07",memory:64,price:22000},
-  {name:"Xiomi",model:"note",memory:128,price:12000},
-  {name:"apple",model:"s01",memory:64,price:2500},
-  {name:"motorola",model:"s01",memory:64,price:15000}
+var data = [
+  { name: "samsung", model: "Galaxy", memory: 32, price: 15000 },
+  { name: "nokia", model: "S07", memory: 64, price: 22000 },
+  { name: "Xiomi", model: "note", memory: 128, price: 12000 },
+  { name: "apple", model: "s01", memory: 64, price: 2500 },
+  { name: "motorola", model: "s01", memory: 64, price: 15000 },
+];
 
-]
-
-function add(){
-
-var Company=document.getElementById("Company").value;
-var Model=document.getElementById("Model").value;
-var Memory=document.getElementById("Memory").value;
-var price=document.getElementById("price").value;
-    var list={
-          name:Company,
-          memory:Memory,
-          model:Model,
-          price:price
-    }
-    arr1.push(list)
-    console.log(arr1)
-if(!isNaN(Company)){
-  document.getElementById("p1").innerHTML="plss Enter comapny name in string.";
-}
-else{
-  
-  var table="<table><th>Companny</th><th>Model</th><th>Memory</th><th>price</th></tr>"
-       arr1.forEach(element => {
-        table+="<tr><td>"+
-        element.name+
-        "</td><td>"+
-        element.model+
-        "</td><td>"+   
-        element.memory+
-        "</td><td>"+
-        element.price+     
-          "</td></tr>"
-  });
-  table+="</table>"
-  document.getElementById("p1").innerHTML=table;
-}
+function add() {
+  var list = {
+    name: document.getElementById("Company").value,
+    memory: document.getElementById("Model").value,
+    model: document.getElementById("Memory").value,
+    price: document.getElementById("price").value,
+  };
+  data.splice(1, 0, list);
+  if (!isNaN(Company)) {
+    document.getElementById("p1").innerHTML =
+      "plss Enter comapny name in string.";
+  } else {
+    var table =
+      "<table><th>Companny</th><th>Model</th><th>Memory</th><th>price</th></tr>";
+    data.forEach((element) => {
+      table +=
+        "<tr><td>" +
+        element.name +
+        "</td><td>" +
+        element.model +
+        "</td><td>" +
+        element.memory +
+        "</td><td>" +
+        element.price +
+        "</td></tr>";
+    });
+    table += "</table>";
+    document.getElementById("p1").innerHTML = table;
+  }
 }
