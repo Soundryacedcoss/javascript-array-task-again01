33abf7fc3c1a5b0e637ca60787395d8776a7152e
Soundryacedcoss <soundryarani@cedcoss.com>
2022-10-06 18:47:07 +0530

    modified in add item in 2nd position

--- javascript-array4/script.js
+++ javascript-array4/script.js
@@ -13,11 +13,58 @@ function add() {
     model: document.getElementById("Memory").value,
     price: document.getElementById("price").value,
   };
-  data.splice(1, 0, list);
-  if (!isNaN(Company)) {
+  if(list.name==""&& list.memory=="" && list.model=="" && list.price=="")
+  {
+    document.getElementById("p1").innerHTML =
+      "plss Enter all details";
+      console.log("hi");
+  }
+  else if(list.name==""&& list.memory=="" && list.model==""){
+    {
+      document.getElementById("p1").innerHTML =
+        "plss Enter name memory and model";
+    }
+  }
+  else if(list.name=="" && list.memory=="" && list.price==""){
+    {
+      document.getElementById("p1").innerHTML =
+        "plss Enter name memory and price";
+    }
+  }
+  else if(list.name=="" && list.memory){
+    document.getElementById("p1").innerHTML =
+      "plss Enter name and memory";
+  }
+  else if(list.name=="" && list.model){
+    document.getElementById("p1").innerHTML =
+      "plss Enter name and model";
+  }
+  else if(list.name=="" && list.price){
+    document.getElementById("p1").innerHTML =
+      "plss Enter name and price";
+  }
+  else if(list.name==""){
+    document.getElementById("p1").innerHTML =
+      "plss Enter name";
+  }
+  else if(list.memory==""){
+    document.getElementById("p1").innerHTML =
+      "plss Enter memory";
+  }
+  else if(list.price==""){
+    document.getElementById("p1").innerHTML =
+      "plss Enter price";
+  }
+  else if(list.model==""){
+    document.getElementById("p1").innerHTML =
+      "plss Enter model";
+  }
+  else if (!isNaN(list.name)) {
     document.getElementById("p1").innerHTML =
       "plss Enter comapny name in string.";
-  } else {
+  } 
+  else{
+    data.splice(1, 0, list);
     var table =
       "<table><th>Companny</th><th>Model</th><th>Memory</th><th>price</th></tr>";
     data.forEach((element) => {
